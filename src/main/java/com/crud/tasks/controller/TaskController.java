7e5fb93c73502fa15eb29d43bc266482b556fe3d
RFLewandowski <R.F.Lewandowski@gmail.com>
2017-11-12 23:09:15 +0100

    started adding test methods to TaskControllerTestSuite

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,7 +4,6 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.domain.TaskNotFoundException;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.TaskService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
--- src/test/java/com/crud/tasks/TaskControllerTestSuite.java
+++ src/test/java/com/crud/tasks/TaskControllerTestSuite.java
@@ -1,7 +1,10 @@
 package com.crud.tasks;
 
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskNotFoundException;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.TaskService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +21,55 @@ public class TaskControllerTestSuite {
     @Autowired
     private TaskMapper taskMapper;
 
-
-
-
     @Test
     public void ShouldLoadContext() {
+    }
+
+    @Test
+    public void ShouldSaveToH2Database() throws TaskNotFoundException {
+        //Given
+        Task savedTask = new Task((long) 1, "testTitleH2Db", "dummyContent");
+        taskService.saveTask(savedTask);
+
+        //When
+        Task recoveredTask = taskService.getTask((long) 1)
+                .orElseThrow(TaskNotFoundException::new);
+        //Then
+        Assert.assertEquals("testTitleH2Db", recoveredTask.getTitle());
+    }
+
+    @Test
+    public void ShouldGETTasks() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    public void ShouldGETOneTask() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    public void ShouldDELETETask() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    public void ShouldPUTTask() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    public void ShouldPOSTTask() {
+        //Given
+        //When
+        //Then
     }
 }
