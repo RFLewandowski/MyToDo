919f5d6254ae9fe4e17d9bf4177c3ca188e26823
RFLewandowski <R.F.Lewandowski@gmail.com>
2017-12-03 23:51:22 +0100

    Spring @Value now works as it should

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -4,6 +4,7 @@ import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -18,17 +19,17 @@ public class TrelloClient {
 
     private final RestTemplate restTemplate;
 
-    //@Value("${trello.app.username}")//@Value annotation not working
-    private String trelloUsername = "rdim1";
+    @Value("${trello.app.username}")
+    private String trelloUsername;
 
-    //@Value("${trello.api.endpoint.prod}")//@Value annotation not working
-    private String trelloApiEndpoint = "https://api.trello.com/1";
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
 
-    //@Value("${trello.app.key}")//@Value annotation not working
-    private String trelloAppKey = "87f5af154471db449dcf1174bf34890f";
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
 
-    //@Value("${trello.app.token}")//@Value annotation not working
-    private String trelloToken = "7e0d86ef6f2816b6d0c92ae9ce54ef5a691e171515a0f2fd4e66395a38fbb01d";
+    @Value("${trello.app.token}")
+    private String trelloToken;
 
     @Autowired
     public TrelloClient(RestTemplate restTemplate) {
--- src/test/java/com/crud/tasks/TaskControllerTestSuite.java
+++ src/test/java/com/crud/tasks/TaskControllerTestSuite.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,7 +24,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+@TestPropertySource("classpath:test.properties")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
